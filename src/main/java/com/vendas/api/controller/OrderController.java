79ac20cf8de373a131e4dc871d51b06ef5e29039
luizPadial <luizpadial10@gmail.com>
2024-04-22 21:08:52 -0300

    Atualizado OrderController para receber informações das classesDTO

--- src/main/java/com/vendas/api/controller/OrderController.java
+++ src/main/java/com/vendas/api/controller/OrderController.java
@@ -1,12 +1,21 @@
 package com.vendas.api.controller;
 
+import com.vendas.api.mapper.OrderMapper;
+import com.vendas.api.mapper.SellerMapper;
+import com.vendas.api.requestDTO.OrderRequest;
+import com.vendas.api.requestDTO.SellerRequest;
+import com.vendas.api.responseDTO.OrderResponse;
+import com.vendas.api.responseDTO.SellerResponse;
 import com.vendas.domain.model.Order;
+import com.vendas.domain.model.Order;
+import com.vendas.domain.model.Seller;
 import com.vendas.domain.services.OrderService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
@@ -15,18 +24,41 @@ public class OrderController {
 
 
     private OrderService orderService;
+//    @GetMapping()
+//    public ResponseEntity<List<Order>> listOrders(){
+//        return ResponseEntity.status(200).body(orderService.listOrders());
+//    }
+
     @GetMapping()
-    public ResponseEntity<List<Order>> listOrders(){
-        return ResponseEntity.status(200).body(orderService.listOrders());
+    public ResponseEntity<List<OrderResponse>> listOrders(){
+        List<Order> orders = orderService.listOrders();
+        List<OrderResponse> orderResponses = OrderMapper.toOrderResponseList(orders);
+        return ResponseEntity.status(200).body(orderResponses);
     }
+//    @PostMapping
+//    public ResponseEntity<Order> createOrder(@RequestBody Order order){
+//        return ResponseEntity.status(201).body(orderService.createOrder(order));
+//    }
+
     @PostMapping
-    public ResponseEntity<Order> createOrder(@RequestBody Order order){
-        return ResponseEntity.status(201).body(orderService.createOrder(order));
+    public ResponseEntity<OrderResponse> createOrder(@RequestBody OrderRequest request){
+        Order order = OrderMapper.toOrder(request);
+        Order orderCreated =orderService.createOrder(order);
+        OrderResponse orderResponse = OrderMapper.toOrderResponse(orderCreated);
+        return ResponseEntity.status(201).body(orderResponse);
     }
 
+//    @PutMapping
+//    public ResponseEntity<Order> editOrder(@RequestBody Order order){
+//        return ResponseEntity.status(200).body(orderService.editOrder(order));
+//    }
+
     @PutMapping
-    public ResponseEntity<Order> editOrder(@RequestBody Order order){
-        return ResponseEntity.status(200).body(orderService.editOrder(order));
+    public ResponseEntity<OrderResponse> editOrder(@RequestBody OrderRequest request){
+        Order order = OrderMapper.toOrder(request);
+        Order orderCreated =orderService.createOrder(order);
+        OrderResponse orderResponse = OrderMapper.toOrderResponse(orderCreated);
+        return ResponseEntity.status(200).body(orderResponse);
     }
 
     @DeleteMapping("/{id}")
@@ -35,9 +67,18 @@ public class OrderController {
         return ResponseEntity.status(204).build();
     }
 
+//    @GetMapping(value = "/{id}")
+//    public ResponseEntity<Order> findById(@PathVariable Long id){
+//        return ResponseEntity.ok().body(orderService.buscarPorId(id));
+//    }
+
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Order> findById(@PathVariable Long id){
-        return ResponseEntity.ok().body(orderService.buscarPorId(id));
+    public ResponseEntity<Optional<OrderResponse>> findById(@PathVariable Long id){
+        Optional<Order> optOrder = Optional.ofNullable(orderService.buscarPorId(id));
+        if(optOrder.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.status(200).body(Optional.of(OrderMapper.toOrderResponse(optOrder.get())));
     }
 
 
--- src/main/java/com/vendas/api/mapper/OrderMapper.java
+++ src/main/java/com/vendas/api/mapper/OrderMapper.java
@@ -1,7 +1,13 @@
 package com.vendas.api.mapper;
 
 import com.vendas.api.requestDTO.OrderRequest;
+import com.vendas.api.responseDTO.OrderResponse;
+import com.vendas.api.responseDTO.SellerResponse;
 import com.vendas.domain.model.Order;
+import com.vendas.domain.model.Seller;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderMapper {
 
@@ -13,5 +19,22 @@ public class OrderMapper {
         return order;
     }
 
+    public static OrderResponse toOrderResponse(Order order){
+        OrderResponse response = new OrderResponse();
+        response.setId(order.getId());
+        response.setDate(order.getDate());
+        response.setPrice(order.getPrice());
+        response.setSeller(order.getSeller());
+        return response;
+    }
+
+
+    public static List<OrderResponse> toOrderResponseList(List<Order> orders) {
+        List<OrderResponse> responses = new ArrayList<>();
+        for (Order order : orders){
+            responses.add(toOrderResponse(order));
+        }
+        return responses;
+    }
 
 }
